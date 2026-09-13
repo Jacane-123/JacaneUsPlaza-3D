ac153ba04f556febeee464867e9b6db100fb02f8
JacaneU <bavajacopo@gmail.com>
2026-09-13 14:27:28 +0200

    Update miiMesh rotation calculation

--- main.js
+++ main.js
@@ -286,7 +286,7 @@ function animate( time ) {
   // player
   if ( miiMesh ) {
     miiMesh.position.copy( player.position );
-    miiMesh.rotation.y = player.rotation.y + 90;
+    miiMesh.rotation.y = player.rotation.y + Math.PI;
     miiMesh.position.y = player.position.y + 1.35;
   }
   if ( username ) {
